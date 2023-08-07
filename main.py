39b37ab3e4e0bffd698d8b22d30e8d0ee7378119
Tom <tapontikes@gmail.com>
2023-08-07 14:15:17 -0400

    Adjusted template and re-worked logic

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import os, requests, openpyxl
+import openpyxl
+import os
+import requests
+
+from docx import Document
+from docxcompose.composer import Composer
 from docxtpl import DocxTemplate
 from dotenv import load_dotenv
-from docxcompose.composer import Composer
-from docx import Document
 
 
 def merge_documents(temp_path, output_path):
@@ -38,33 +41,59 @@ def get_sheet():
     return response.content
 
 
+def get_int(num):
+    try:
+        return int(num)
+    except Exception:
+        return None
+
+
 def iter_rows():
     wb = openpyxl.load_workbook("guest.xlsx")
-    # Get the first sheet
     ws = wb.active
 
     data = []
 
     for i in range(5, ws.max_row + 1):
-        cells = ["" if cell.value is None else cell.value for cell in ws[i]]
 
-        if cells[0] == "Print" and cells[14] == "F":
-            data.append({
-                "PREFIX": cells[1],
-                "SUFFIX": "",
-                "NAME": cells[2] + " " + cells[3],
-                "ADDRESS1": cells[9],
-                "ADDRESS2": cells[10],
-                "CITY": cells[11],
-                "STATE": cells[12],
-                "ZIP": cells[13]
-            })
+        cells = [" " if cell.value is None else cell.value for cell in ws[i]]
+
+        print_state = cells[0]
+        prefix = cells[1]
+        first_name = cells[2]
+        last_name = cells[3]
+        suffix = cells[8]
+        address_one = cells[9]
+        address_two = cells[10]
+        city = cells[11]
+        state = cells[12]
+        zip_code = cells[13]
+        complete_status = cells[14]
+        partner_ref = get_int(cells[15])
+
+        if print_state == "Print" and complete_status == "F":
+            names = [str.join(" ", [prefix, first_name, last_name, suffix]).strip(" ")]
+            # If partner ref is found, members living together but unwed
+            if partner_ref:
+                for partner_row in ws.iter_rows(min_row=5):
+                    if get_int(partner_row[16].value) == partner_ref:
+                        names.append(str.join(" ", [partner_row[1].value, partner_row[2].value,partner_row[3].value]).strip(" "))
+                        break
+            data.append({"NAMES": names,"ADDRESS1": address_one,"ADDRESS2": address_two,"CITY": city,"STATE": state,"ZIP": zip_code})
+
     return data
 
 
-for file in os.listdir("tmp"):
-    if file:
-        os.remove(os.path.join("tmp", file))
+try:
+    for file in os.listdir("tmp"):
+        if file:
+            os.remove(os.path.join("tmp", file))
+
+    os.remove("guest.xlsx")
+    os.remove(os.path.join("dist", "complete.docx"))
+
+except Exception as e:
+    print(e)
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
